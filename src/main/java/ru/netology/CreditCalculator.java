b8ad7bb2b51bea54ea614c38cc6c08a8db09ecb5
Kirillov Alexander <kirillovbrest@gmail.com>
2021-03-24 16:28:13 +0300

    Initial commit. Tests upd

---
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import java.math.BigDecimal;
+
+public class CreditCalculator {
+
+    public final static int ROUNDING_MODE = BigDecimal.ROUND_HALF_UP;
+
+    public static BigDecimal monthlyPayment(int sum, int month, double percent) {
+        return new BigDecimal(-1);
+    }
+
+    public static BigDecimal totalReturnedSum(int sum, int month, double percent) {
+        return new BigDecimal(-1);
+    }
+
+    public static BigDecimal totalOverpayment(int sum, int month, double percent) {
+        return new BigDecimal(-1);
+    }
+}
---
+++ src/test/java/ru/netology/CreditCalculatorTest.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class CreditCalculatorTest{
+
+    private static int ROUNDING_MODE;
+
+    @BeforeAll
+    public static void before() {
+        ROUNDING_MODE = CreditCalculator.ROUNDING_MODE;
+    }
+    @Test
+    public void testMonthlyPayment() {
+        BigDecimal expected = new BigDecimal(91.67).setScale(2, ROUNDING_MODE);
+        assertThat(CreditCalculator.monthlyPayment(1000, 12, 0.1), is(equalTo(expected)));
+    }
+
+    @Test
+    public void testTotalReturnedSum() {
+        BigDecimal expected = new BigDecimal(1100).setScale(2, ROUNDING_MODE);
+        assertThat(CreditCalculator.totalReturnedSum(1000, 12, 0.1), is(equalTo(expected)));
+    }
+
+    @Test
+    public void testTotalOverpayment() {
+        BigDecimal expected = new BigDecimal(100).setScale(2, ROUNDING_MODE);
+        assertThat(CreditCalculator.totalOverpayment(1000, 12, 0.1), is(equalTo(expected)));
+    }
+}
